18c12c724ecd3e17552b145907df55adf44ccf57
unknown <syedtalhaahmed586@gmail.com>
2024-07-03 11:01:47 +0500

    changes

--- src/screens/DeniedByCardholder.js
+++ src/screens/DeniedByCardholder.js
@@ -149,7 +149,7 @@ const DeniedByCardholder = () => {
   useEffect(() => {
 
     GetDeniedByHourCardholders();
-  }, []);
+  }, [selectedPersonCardNo]);
 
   return (
     <View style={styles.container}>
