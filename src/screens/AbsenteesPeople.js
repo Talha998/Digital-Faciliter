a56582d4e973f65226bb4c4bcd5442cbddad7701
unknown <syedtalhaahmed586@gmail.com>
2024-07-04 11:58:11 +0500

    changes

--- src/screens/AbsenteesPeople.js
+++ src/screens/AbsenteesPeople.js
@@ -1,172 +1,272 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Modal, ScrollView, TextInput } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Modal, ScrollView, TextInput, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import DatePicker from 'react-native-date-picker';
+import DropDownPicker from 'react-native-dropdown-picker';
+import axios from 'axios';
+import { AppContext } from '../Context/AppContext';
 
 const AbsenteesPeople = () => {
   const headers = ['Person ID', 'Card Number', 'Person Name', 'Department', 'Designation', 'Level 4', 'Entry Time'];
-  const [fromDate, setFromDate] = useState(new Date());
-  const [OnlyDate, setOnlyDate] = useState(new Date());
-  const [toDate, setToDate] = useState(new Date());
+  const {
+    selectedRegion,
+    selectedCity,
+    selectedLocation,
+    selectedArea,
+    selectedBrand,
+  } = useContext(AppContext);
+
+  const currentDate = new Date();
+  const fromDateTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0, 0);
+  const toDateTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 23, 59, 59);
+
+  const [onlyDate, setOnlyDate] = useState(currentDate);
+  const [fromDate, setFromDate] = useState(fromDateTime);
+  const [toDate, setToDate] = useState(toDateTime);
   const [modalVisible, setModalVisible] = useState(false);
-  
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [emergencypageNumber, setEmergencypageNumber] = useState(0);
+const [emergencypageSize, setEmergencypageSize] = useState(10);
+const [totalEmergencyRecords, setTotalEmergencyRecords] = useState(0);
+const [pageSizeItems, setPageSizeItems] = useState([
+  { label: '10', value: 10 },
+  { label: '25', value: 25 },
+  { label: '50', value: 50 },
+  { label: '100', value: 100 }
+]);
+
+  const fetchAttendanceData = async () => {
+    setLoading(true);
+    try {
+      const baseUrl = await AsyncStorage.getItem('baseURL');
+      const token = await AsyncStorage.getItem('userToken');
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      const response = await axios.get(`${baseUrl}/api/GetAttendAbsentees`, {
+        params: {
+          Language: "p",
+          Level1_ID: selectedRegion,
+          Level2_ID: selectedCity,
+          Level3_ID: selectedLocation,
+          Level4_ID: selectedArea,
+          Eqpt_Group_ID: selectedBrand,
+          EventDate: convertToLocalTime(onlyDate),
+          StartTime: convertToLocalTime(fromDate),
+          EndTime: convertToLocalTime(toDate),
+          Filter_By: "A",
+          Filter_Text: "None",
+          Page_Number: pageNumber + 1,
+          Page_Size: pageSize,
+          "api-version": "1.0"
+        },
+        headers: headers
+      });
+      if (response.data.StatusCode === 200) {
+      setData(response.data.Data.Data);
+      setFilteredData(response.data.Data.Data);
+      setTotalEmergencyRecords(response.data.Data.TotalCount);
+      setLoading(false);
+    } else {
+      console.error('Error fetching data:', response.data.Message);
+      setData([]);
+      setFilteredData([]);
+      setTotalEmergencyRecords(0);
+      setLoading(false);
+  }
+} catch (error) {
+  console.error('Error fetching data:', error);
+  setData([]);
+  setFilteredData([]);
+  setTotalEmergencyRecords(0);
+  setLoading(false);
+}
+  };
+
+  useEffect(() => {
+    fetchAttendanceData();
+  }, [pageNumber, pageSize, onlyDate, fromDate, toDate , selectedRegion, selectedCity, selectedLocation, selectedArea, selectedBrand]);
+
+  const handleSearch = (text) => {
+    setSearchQuery(text);
+    if (text.trim() === '') {
+      setFilteredData(data); // Reset to original data if search query is empty
+    } else {
+      const filtered = data.filter(item => item.Person_Name.toLowerCase().includes(text.toLowerCase()));
+      setFilteredData(filtered);
+    }
+  };
+
   const handleConfirm = () => {
     setModalVisible(false);
+    fetchAttendanceData(); // Fetch data based on new date range
   };
 
   const handleCancel = () => {
     setModalVisible(false);
   };
 
-  const initialData = [
-    { id: '1', cardNumber: '1234', name: 'John', department: 'HR', designation: 'Manager', level4: 'L4-A', entryTime: '09:00 AM' },
-    { id: '2', cardNumber: '1234', name: 'Jane', department: 'Finance', designation: 'Analyst', level4: 'L4-B', entryTime: '09:15 AM' },
-    { id: '3', cardNumber: '1234', name: 'Jake', department: 'Engineering', designation: 'Engineer', level4: 'L4-C', entryTime: '10:00 AM' },
-    // Add more data objects here
-  ];
-
-  const [data, setData] = useState(initialData);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [filteredData, setFilteredData] = useState(initialData);
-
-  const personIdWidth = 80;
-  const cardNumberWidth = 110;
-  const PersonNameWidth = 110;
-  const DepartmentWidth = 100;
-  const DesignationWidth = 100;
-  const Level4Width = 100;
-  const EntryTimeWidth = 100;
-
-  // Function to handle search input change
-  const handleSearch = (text) => {
-    setSearchQuery(text);
-    if (text.trim() === '') {
-      setFilteredData(data); // Reset to original data if search query is empty
-    } else {
-      const filtered = data.filter(item => item.name.toLowerCase().includes(text.toLowerCase()));
-      setFilteredData(filtered);
-    }
+  const convertToLocalTime = (date) => {
+    const localDate = new Date(date.getTime() - (date.getTimezoneOffset() * 60000));
+    return localDate.toISOString().slice(0, 19).replace('T', ' ');
   };
 
+  const handlePageChange = (newPageNumber) => {
+    if (newPageNumber >= 0 && newPageNumber < Math.ceil(totalRecords / pageSize)) {
+      setPageNumber(newPageNumber);
+    }
+  };
+  const onPageChange = (newPageNumber) => {
+    setEmergencypageNumber(newPageNumber);
+};
+
+const onIncomingpageSizeChange = (itemValue) => {
+    setEmergencypageSize(itemValue);
+    setEmergencypageNumber(0); // Reset to first page on page size change
+};
+
   return (
-   <View style={styles.container}>
-  <View style={styles.topContainer}>
-    <View>
-      <View style={styles.dateContainer}>
-        <Text style={styles.label_from}>Date : </Text>
-        <Text style={styles.dateText}>{`${OnlyDate.toLocaleDateString()}`}</Text>
-      </View>
-      <View style={styles.dateContainer}>
-        <Text style={styles.label_from}>From : </Text>
-        <Text style={styles.dateText}>{`${fromDate.toLocaleTimeString()}`}</Text>
-      </View>
-      <View style={styles.dateContainer}>
-        <Text style={styles.label_To}>To : </Text>
-        <Text style={styles.dateText}>{`${toDate.toLocaleTimeString()}`}</Text>
-      </View>
-    </View>
-    <View style={styles.right_icons}>
-      <TouchableOpacity
-        onPress={() => setModalVisible(true)}
-        style={styles.openModalButton}
-      >
-        <Icon name="calendar" size={28} color="#00544d" />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Icon name="refresh" size={28} color="#00544d" />
-      </TouchableOpacity>
-    </View>
-  </View>
-
-  <Modal
-    transparent={true}
-    visible={modalVisible}
-    animationType="slide"
-    onRequestClose={() => setModalVisible(false)}
-  >
-    <View style={styles.modalBackground}>
-      <View style={styles.modalContainer}>
-        <View style={styles.serverURLContainerf2}>
-          <Text style={styles.modalTitle}>Select Dates</Text>
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <View>
+          <View style={styles.dateContainer}>
+            <Text style={styles.label_from}>Date : </Text>
+            <Text style={styles.dateText}>{onlyDate.toLocaleDateString()}</Text>
+          </View>
+          <View style={styles.dateContainer}>
+            <Text style={styles.label_from}>From : </Text>
+            <Text style={styles.dateText}>{fromDate.toLocaleTimeString()}</Text>
+          </View>
+          <View style={styles.dateContainer}>
+            <Text style={styles.label_To}>To : </Text>
+            <Text style={styles.dateText}>{toDate.toLocaleTimeString()}</Text>
+          </View>
         </View>
-        <View style={styles.datePickerContainer}>
-          <Text style={styles.label_dash}>Date: </Text>
-          <DatePicker
-                date={OnlyDate}
-                onDateChange={setOnlyDate}
-                mode="date"
-                style={styles.datePicker}
-                is24hourSource="locale"
-              />
-        </View>
-        <View style={styles.datePickerContainer}>
-          <Text style={styles.label_dash}>From Time: </Text>
-          <DatePicker
-            date={fromDate}
-            onDateChange={date => setFromDate(new Date(date))}
-            mode="time"
-            style={styles.datePicker}
-            is24hourSource="locale"
-          />
-        </View>
-        <View style={styles.datePickerContainer}>
-          <Text style={styles.label_dash}>To Time: </Text>
-          <DatePicker
-            date={toDate}
-            onDateChange={date => setToDate(new Date(date))}
-            mode="time"
-            style={styles.datePicker}
-            is24hourSource="locale"
-          />
-        </View>
-        <View style={styles.modalButtons}>
-          <TouchableOpacity
-            onPress={handleCancel}
-            style={[styles.modalButton, styles.cancelButton]}
-          >
-            <Text style={styles.cancelButtonText}>Cancel</Text>
+        <View style={styles.right_icons}>
+          <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.openModalButton}>
+            <Icon name="calendar" size={28} color="#00544d" />
           </TouchableOpacity>
-          <TouchableOpacity
-            onPress={handleConfirm}
-            style={[styles.modalButton, styles.confirmButton]}
-          >
-            <Text style={styles.confirmButtonText}>Confirm</Text>
+          <TouchableOpacity onPress={fetchAttendanceData}>
+            <Icon name="refresh" size={28} color="#00544d" />
           </TouchableOpacity>
         </View>
       </View>
-    </View>
-  </Modal>
 
-  <TextInput
-    style={styles.searchInput}
-    placeholder="Search by Person Name"
-    onChangeText={handleSearch}
-    value={searchQuery}
-  />
+      <Modal transparent={true} visible={modalVisible} animationType="slide" onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Select Dates</Text>
+            <View style={styles.datePickerContainer}>
+              <Text style={styles.label_dash}>Date: </Text>
+              <DatePicker date={onlyDate} onDateChange={setOnlyDate} mode="date" style={styles.datePicker} />
+            </View>
+            <View style={styles.datePickerContainer}>
+              <Text style={styles.label_dash}>From Time: </Text>
+              <DatePicker date={fromDate} onDateChange={setFromDate} mode="time" style={styles.datePicker} />
+            </View>
+            <View style={styles.datePickerContainer}>
+              <Text style={styles.label_dash}>To Time: </Text>
+              <DatePicker date={toDate} onDateChange={setToDate} mode="time" style={styles.datePicker} />
+            </View>
+            <View style={styles.modalButtons}>
+              <TouchableOpacity onPress={handleCancel} style={[styles.modalButton, styles.cancelButton]}>
+                <Text style={styles.cancelButtonText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleConfirm} style={[styles.modalButton, styles.confirmButton]}>
+                <Text style={styles.confirmButtonText}>Confirm</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
 
-  <ScrollView horizontal>
-    <View style={styles.table}>
-      <View style={styles.headerRow}>
-        <Text style={[styles.headerCell, { width: personIdWidth, borderRightWidth: 1, borderColor: '#fff' }]}>Person ID</Text>
-        <Text style={[styles.headerCell, { width: cardNumberWidth, borderRightWidth: 1, borderColor: '#fff' }]}>Card Number</Text>
-        <Text style={[styles.headerCell, { width: PersonNameWidth, borderRightWidth: 1, borderColor: '#fff' }]}>Person Name</Text>
-        <Text style={[styles.headerCell, { width: DepartmentWidth, borderRightWidth: 1, borderColor: '#fff' }]}>Department</Text>
-      </View>
-      {filteredData.length > 0 ? (
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search by Person Name"
+        onChangeText={handleSearch}
+        value={searchQuery}
+      />
+
+     
+        <ScrollView horizontal>
+          <View style={styles.table}>
+            <View style={styles.headerRow}>
+              <Text style={[styles.headerCell, { width: 80, borderRightWidth: 1, borderColor: '#fff' }]}>Person ID</Text>
+              <Text style={[styles.headerCell, { width: 110, borderRightWidth: 1, borderColor: '#fff' }]}>Card Number</Text>
+              <Text style={[styles.headerCell, { width: 110, borderRightWidth: 1, borderColor: '#fff' }]}>Person Name</Text>
+              <Text style={[styles.headerCell, { width: 100, borderRightWidth: 1, borderColor: '#fff' }]}>Department</Text>
+            </View>
+            <ScrollView style={styles.scrollView}>
+      {loading ? (
+        <ActivityIndicator style={styles.loadingIndicator} size="large" color="#00544d" />
+      ) : filteredData.length > 0 ? (
         filteredData.map((item, index) => (
           <View key={index} style={styles.dataRow}>
-            <Text style={[styles.dataCell, { width: personIdWidth, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.id}</Text>
-            <Text style={[styles.dataCell, { width: cardNumberWidth, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.cardNumber}</Text>
-            <Text style={[styles.dataCell, { width: PersonNameWidth, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.name}</Text>
-            <Text style={[styles.dataCell, { width: DepartmentWidth, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.department}</Text>
+            <Text style={[styles.dataCell, { width: 80, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.Person_ID}</Text>
+            <Text style={[styles.dataCell, { width: 110, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.Person_Card_No}</Text>
+            <Text style={[styles.dataCell, { width: 110, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.Person_Name}</Text>
+            <Text style={[styles.dataCell, { width: 100, borderRightWidth: 1, borderColor: '#ddd' }]}>{item.Department_Title}</Text>
           </View>
         ))
       ) : (
         <Text style={styles.noDataText}>No data found</Text>
       )}
+    </ScrollView>
+          </View>
+        </ScrollView>
+     
+    <View style={styles.pagination}>
+                <TouchableOpacity 
+                    onPress={() => onPageChange(0)} 
+                    disabled={emergencypageNumber === 0}
+                    style={[styles.button, emergencypageNumber === 0 && styles.disabledButton]}
+                >
+                    <Text style={styles.buttonText}>{"<<"}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={() => onPageChange(emergencypageNumber - 1)} 
+                    disabled={emergencypageNumber === 0}
+                    style={[styles.button, emergencypageNumber === 0 && styles.disabledButton]}
+                >
+                    <Text style={styles.buttonText}>{"<"}</Text>
+                </TouchableOpacity>
+                <Text style={styles.pageNumber}>
+                    Page {emergencypageNumber + 1} of {Math.ceil(totalEmergencyRecords / emergencypageSize)}
+                </Text>
+                <TouchableOpacity 
+                    onPress={() => onPageChange(emergencypageNumber + 1)} 
+                    disabled={emergencypageNumber === Math.ceil(totalEmergencyRecords / emergencypageSize) - 1}
+                    style={[styles.button, emergencypageNumber === Math.ceil(totalEmergencyRecords / emergencypageSize) - 1 && styles.disabledButton]}
+                >
+                    <Text style={styles.buttonText}>{">"}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={() => onPageChange(Math.ceil(totalEmergencyRecords / emergencypageSize) - 1)} 
+                    disabled={emergencypageNumber === Math.ceil(totalEmergencyRecords / emergencypageSize) - 1}
+                    style={[styles.button, emergencypageNumber === Math.ceil(totalEmergencyRecords / emergencypageSize) - 1 && styles.disabledButton]}
+                >
+                    <Text style={styles.buttonText}>{">>"}</Text>
+                </TouchableOpacity>
+            </View>
+            <DropDownPicker
+                open={open}
+                value={emergencypageSize}
+                items={pageSizeItems}
+                setOpen={setOpen}
+                setValue={setEmergencypageSize}
+                setItems={setPageSizeItems}
+                containerStyle={styles.dropdownContainer}
+                onChangeValue={onIncomingpageSizeChange}
+            />
     </View>
-  </ScrollView>
-</View>
   );
 };
 
@@ -177,6 +277,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 18,
     backgroundColor: '#fff',
   },
+  scrollView: {
+    maxHeight: 420, // Adjust the maximum height as per your requirement
+},
+loadingIndicator: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop:"30%"
+}, 
   searchInput: {
     height: 40,
     borderColor: '#ddd',
@@ -331,6 +440,32 @@ const styles = StyleSheet.create({
     width: 120,
     textAlign: "center",
   },
+  pagination: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+},
+button: {
+  padding: 10,
+  backgroundColor: '#004d40',
+  marginHorizontal: 5,
+  borderRadius: 5,
+},
+disabledButton: {
+  backgroundColor: '#ccc',
+},
+buttonText: {
+  color: '#fff',
+},
+pageNumber: {
+  marginHorizontal: 10,
+  fontSize: 16,
+},
+dropdownContainer: {
+    width: 150,
+    marginTop: 10,
+},
 });
 
 export default AbsenteesPeople;
